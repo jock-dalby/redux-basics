7813fcb1338cda40f8a4dfc6beab85c706486dbc
jock-dalby <jockdalby86@gmail.com>
2018-05-28 18:27:50 +1200

    set up increment counter and add counter in reducer and dispatch actions

--- redux-basics.js
+++ redux-basics.js
@@ -2,22 +2,46 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const initialState = {
-  name: 'Jock'
+  counter: 0
 };
 
 /*
  * REDUCER
- * - The only thing that may update state
- * - Just a function that takes in current state and an action
+ * - The only thing that may update state.
+ * - Just a function that takes in current state and an action.
  */
 const rootReducer = (state = initialState, action) => {
+  if (action.type === 'INCREMENT_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + 1
+    }
+  }
+  if (action.type === 'ADD_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    }
+  }
   return state;
 }
 
-// Store => Must be initialised with reducer
+/**
+ * STORE
+ * - Must be initialised with reducer.
+ */
 const store = createStore(rootReducer);
-console.log(store.getState());
+console.log('Before actions are dispatched', store.getState());
 
-// Dispatch action
+/*
+ * DISPATCH ACTION
+ * - Action is JS object, MUST have type prop.
+ * - Payload (and other props) can be named however.
+ */
+store.dispatch({type: 'INCREMENT_COUNTER'});
+console.log('After INCREMENT_COUNTER action is dispatched', store.getState());
+
+store.dispatch({type: 'ADD_COUNTER', value: 10 });
+console.log('After ADD_COUNTER action is dispatched', store.getState());
 
 // Subscription
