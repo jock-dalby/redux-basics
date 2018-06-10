b326aefddfa86cdcac8d95d672d1892b07901136
jock-dalby <jockdalby86@gmail.com>
2018-06-10 18:47:06 +1200

    setup async action creator using redux-thunk and dispatch sync action to update store when async code is complete

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -11,5 +11,23 @@ export const increment = () => ({ type: INCREMENT });
 export const decrement = () => ({ type: DECREMENT });
 export const addTo = (payload) => ({ type: ADD_TO, payload });
 export const subtractFrom = (payload) => ({ type: SUBTRACT_FROM, payload });
-export const storeResult = (payload) => ({ type: STORE_RESULT, payload });
+
+// Sync action called by async action when it is complete. Only sync actions can update the store.
+export const saveResult = (payload) => {
+  return ({ type: STORE_RESULT, payload });
+}
+
+// Async action - Used to execute async code
+export const storeResult = (payload) => {
+  /* To run async code, return a function which redux-thunk gives access to dispatch action and redux-thunk
+  * will execute the async code and then dispatch new action.
+  */
+  return dispatch => {
+    // To simulate asychronous code
+    setTimeout(() => {
+      // When async code has complete, reduc-thunk calls a new action creator
+      dispatch(saveResult(payload));
+    }, 2000)
+  }
+}
 export const deleteResult = (payload) => ({ type: DELETE_RESULT, payload });
