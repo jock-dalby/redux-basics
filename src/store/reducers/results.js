5056da1383aff11fa3eb5efde2282ac70acd52e1
jock-dalby <jockdalby86@gmail.com>
2018-06-12 08:56:13 +1200

    clean up reducer in Results.js to have a leaner switch statement

--- src/store/reducers/results.js
+++ src/store/reducers/results.js
@@ -1,23 +1,25 @@
 import * as actions from '../actions/actionTypes';
+import { updateObject } from '../utlitity';
 
 const initialState = {
   results: []
 }
 
+const storeResult = (state, action) => {
+  return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.payload }) })
+}
+
+const deleteResult = (state, action) => {
+  const updatedResults = state.results.filter(result => result.id !== action.payload );
+  return updateObject(state, { results: updatedResults })
+}
+
 const reducer = (state = initialState, action) => {
-  console.log(action)
   switch ( action.type ) {
     case actions.STORE_RESULT:
-      return {
-        ...state,
-        results: state.results.concat({ id: new Date(), value: action.payload * 2 })
-      }
+      return storeResult(state, action)
     case actions.DELETE_RESULT:
-      const updatedResults = state.results.filter(result => result.id !== action.payload );
-      return {
-        ...state,
-        results: updatedResults
-      }
+      return deleteResult(state, action)
     default:
       return state;
   }
